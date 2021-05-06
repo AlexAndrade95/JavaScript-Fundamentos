ea122e5a115e321eb3b9b0b46cb9c116c5daa511
alex andrade <alexneves19955@gmail.com>
2021-05-06 20:14:26 -0300

    commit 02 alterado para JavaScript

---
+++ 3Organização.js
@@ -0,0 +1,6 @@
+/*
+JavaScript
+- Organização
+	.Sentenças de códigos(Terminado por";" ou não).
+	.Bloco de códigos(Entre chaves "{}").
+*/
---
+++ 4VariaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3  //var � palavra reservada para declarar a vari�vel
+let b = 4 // let � uma forma mais moderna para declarar vari�vel
+// preferivel usar let 
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5 //constante, n�o muda ,n�o varia 
+// c = 50
+console.log(c)
+
