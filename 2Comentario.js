a3d2f822bf6e761f60ce50673203a82a8d010954
alex andrade <alexneves19955@gmail.com>
2021-05-06 20:10:03 -0300

    commit alterado para JavaScript

---
+++ 2Comentario.js
@@ -0,0 +1,19 @@
+// Coment�rio de uma linha
+console.log("Linha 1")
+
+/*
+ coment�rio de 
+ m�ltiplas linhas
+*/
+
+/*
+ *Coment�rio de 
+ *m�ltiplas 
+ *linhas
+*/
+
+/*
+ evite comentar o �bvio
+ S� comente o que for necess�rio
+*/
+ 
