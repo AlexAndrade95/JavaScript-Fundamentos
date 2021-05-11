10eb924c99ce5afcb965a5d4961aa0a8e2de5a77
alex andrade <alexneves19955@gmail.com>
2021-05-10 22:36:59 -0300

    commit 06 alterado para JavaScript

--- 9Strings.js
+++ 9Strings.js
@@ -1,40 +1,40 @@
   Strings
 	delimitar por 3 tipos:
 		"" , '', Crase ` 
-funções de exemplo:
+//funï¿½ï¿½es de exemplo:
  const escola = "Cod3r"
  console.log(escola.charAt(4))
-//retornará a letra r , pois ela esta na 4ª colocação na palavra
+//retornarï¿½ a letra r , pois ela esta na 4ï¿½ colocaï¿½ï¿½o na palavra
  console.log(escola.chartCodeAt(3)) 
-//retornará a colocação de 3 dentro da tabela unicode "valor 51"
+//retornarï¿½ a colocaï¿½ï¿½o de 3 dentro da tabela unicode "valor 51"
 console.log(escola.indexOf('3'))
-// retornará se tem o valor 3 na palavra , retornando sua colocação
+// retornarï¿½ se tem o valor 3 na palavra , retornando sua colocaï¿½ï¿½o
 console.log(escola.substring(1))
-//retornará a palavra a partir do indice 1 "excluindo o indice 0"
+//retornarï¿½ a palavra a partir do indice 1 "excluindo o indice 0"
 'od3r'
 console.log(escola.substring(0,3))
-//retornará a palavra a partir do indice 0 e mostrando até 3 valores
+//retornarï¿½ a palavra a partir do indice 0 e mostrando atï¿½ 3 valores
 'Cod'
 console.log('Escola'.concat(escola.concat("!")) ou console.log('Escola'+ escola + "!")
 'Escola Cod3r!'
 console.log(escola.replace(3,'e'))
-//substituirá o 3 por e
+//substituirï¿½ o 3 por e
 'Coder'
 console.log('Ana,Maria,Pedro.split(','))
-//Reornará um Array com três elementos
+//Reornarï¿½ um Array com trï¿½s elementos
 ['Ana','Maria','Pedro']
 Templates Strings // usa-se a crase ` 
 const nome = 'Rebeca'
-const concatenacao = 'Olá' + nome + '!'
+const concatenacao = 'Olï¿½' + nome + '!'
 const template = `
-	Olá 
+	Olï¿½ 
 	s{nome}!`
-console.log(concatenação, template)
-//Retornará a expressão
-Olá
+console.log(concatenaï¿½ï¿½o, template)
+//Retornarï¿½ a expressï¿½o
+Olï¿½
 Rebeca!
-//Expresões
+//Expresï¿½es
 console.log(`1+1 = ${1+1})
-//retornará a expressão 
+//retornarï¿½ a expressï¿½o 
 1+1=2
 
