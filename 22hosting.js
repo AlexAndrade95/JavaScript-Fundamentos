9b707b516fbf4640d9e36abfe3aed4c10e9ed533
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-19 07:43:20 -0300

    commit 17 alterado

--- 22hosting.js
+++ 22hosting.js
@@ -1,18 +1,17 @@
-HOSTING - içamento ("jogar para cima")
-não é necessário ultilizar, mas é fundamental conhecer 
+/*HOSTING - icamento ("jogar para cima")
+ *nao e necessario ultilizar, mas e fundamental conhecer 
+ */
 
 console.log('a =', a)
 var a = 2
 console.log('a =', a)
+//a = undefined
+//a = 2
+//a variavel foi icada (hosting) 
 
-// a = undefined
-   a = 2
-
-a variável foi içada (hosting) 
--------------------------------
 console.log('b =', b)
 let b = 2
 console.log('b =', b)
-
-// b is not defined
-   let NÃO TEM HOSTING !!!!!
+/*b is not defined
+ *let NAO TEM HOSTING !!!!!
+ */
