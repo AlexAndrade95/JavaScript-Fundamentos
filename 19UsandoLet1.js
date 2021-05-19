71e217dba22e55a28fd36d4e5e3e57a756508ce7
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-19 07:41:05 -0300

    commit 14 alterado

--- 19UsandoLet1.js
+++ 19UsandoLet1.js
@@ -1,14 +1,14 @@
-Let
-tem esopo global
-tem escopo de fun��o
-tem escopo de bloco
+/*Let
+ *tem esopo global
+ *tem escopo de funcao
+ *tem escopo de bloco
+ */
 
-let numero = 1
+ let numero = 1
 {
     let numero = 2
     console.log('dentro =', numero)
 }
 console.log('fora =', numero)
 // dentro = 2
-   fora = 1 ; pois o let est�o em escopos diferentes
-              a preferencia � para o escopo menor
+//fora = 1 ; pois o let estao em escopos diferentes a preferencia e´ para o escopo menor
