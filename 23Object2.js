12431aef1ad8237f4d7a7c08c5640c1fe92b304c
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-19 07:44:07 -0300

    commit 18 alterado

--- 23Object2.js
+++ 23Object2.js
@@ -1,18 +1,18 @@
-objeto 2 
+//objeto 2 
 
 console.log(typeof Object)
 //function
-console.log(typeof new Object)//instanciando um object com new (função)
+console.log(typeof new Object)//instanciando um object com new (funcao)
 //object
 
 const Cliente = function() {}
 console.log(typeof Cliente)
 //function
-console.log(typeof new Cliente)//instanciando uma const com new (função)
+console.log(typeof new Cliente)//instanciando uma const com new (funcao)
 //object
 
 class Produto {} // ES 2015 (ES6)
 console.log(typeof Produto)
 //function
-console.log(typeof new Produto())//instanciando uma class com new (função)
+console.log(typeof new Produto())//instanciando uma class com new (funcao)
 //object
