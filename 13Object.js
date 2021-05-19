902e7b9fc96fbc7c6a0f414ca67c692c9170df82
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-18 21:31:56 -0300

    commit 08 alterado

--- 13Object.js
+++ 13Object.js
@@ -1,14 +1,13 @@
-/*
-Object {}
-coleção de chaves e valores
-usa-se chaves {} para escrever objetos
-pode-se usar de forma dinâmica
-*/
+/*Object {}
+ *colecao de chaves e valores
+ *usa-se chaves {} para escrever objetos
+ *pode-se usar de forma dinamica!!
+ */
 
 const prod1 = {}
 prod1.nome = 'Celular Ultra Mega'
 prod1.preco = 4998.90
-prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
+prod1['Desconto Legal'] = 0.40 // evitar atributos com espaco
 
 console.log(prod1)
 // { nome: 'Celular Ultra Mega', preco: 4998.9, 'Desconto Legal': 0.4 }
