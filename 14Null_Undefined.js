bd26e220b8bb57a150fb9c880fb7254d18898106
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-18 21:32:42 -0300

    commit 09 alterado

--- 14Null_Undefined.js
+++ 14Null_Undefined.js
@@ -1,13 +1,14 @@
 /*
-Null & undefined 
-Null é usado para zerar uma referencia
-Atribuiçoes: 
-em object = referencia (mesmo lugar na memória)
-let = valor (mesmo valor da variável)
-Null = ausencia de valor (não aponta pra endereço de memória)
-undefined = não definido
-*/
-----------------------------------------------------
+ *Null & undefined 
+ *Null e usado para zerar uma referencia
+ *Atribuicoes: 
+ *em object = referencia (mesmo lugar na memoria)
+ *let = valor (mesmo valor da variavel)
+ *Null = ausencia de valor (nao aponta pra endereco de memoria)
+ *undefined = nao definido
+ */
+
+ //no browser ...
 const a = {name:'Teste'}
 
 a
@@ -21,37 +22,36 @@ a
 
 {name:"Opa"}
 
-// mudando a ou b , ambos sofrem a mesma mudança,
-por serem objetos ocupam o mesmo lugar na memória
+/*mudando a ou b , ambos sofrem a mesma mudanca,
+ *por serem objetos ocupam o mesmo lugar na memoria 
+ */
 
----------------------------------------------------
-
-let valor // não inicializada
+let valor // nao inicializada
 console.log(valor)
 //undefined
-undefined = declarado mas não inicializado
+undefined //declarado mas nao inicializado
 
-valor = null // ausência de valor
+valor = null // ausencia de valor
 console.log(valor)
 //null
 // console.log(valor.toString()) // Erro!
 
 const produto = {}
 console.log(produto.preco)
-// undefined
+//undefined
 console.log(produto)
-// {}
+//{}
 produto.preco = 3.50
 console.log(produto)
 //{preco: 3.5 }
 
-produto.preco = undefined // evite atribuir undefined
+produto.preco = undefined //evite atribuir undefined
 console.log(!!produto.preco)
-// delete produto.preco
+//delete produto.preco
 console.log(produto)
-// {preco: undefined}
+//{preco: undefined}
 
-produto.preco = null // sem preço
+produto.preco = null //sem preco
 console.log(!!produto.preco)
 console.log(produto)
 // {preco: null }
