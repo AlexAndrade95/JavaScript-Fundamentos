da5ed5e34992d12d77e7ea2cee356f9091e2718b
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-18 22:28:56 -0300

    commit 10 alterado

--- 15FuncaoEmQuaseTudo.js
+++ 15FuncaoEmQuaseTudo.js
@@ -1,8 +1,9 @@
-/* object em Javascript é uma função
-   class em java script também é uma função
-*/
+/*object em Javascript e uma funcao
+ *class em java script tambem e uma funcao
+ */
 
 console.log(typeof Object)
-
+//function
 class Produto {}
 console.log(typeof Produto)
+//function
