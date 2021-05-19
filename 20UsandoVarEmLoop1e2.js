5bef3a398bd0eb62564ca7cc9250e9b2f619958a
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-19 07:41:51 -0300

    commit 15 alterado

--- 20UsandoVarEmLoop1e2.js
+++ 20UsandoVarEmLoop1e2.js
@@ -1,24 +1,25 @@
-var em loop
+//var em loop
 
 for (var i = 0; i < 10; i++) {
     console.log(i)
 }
-//
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-console.log('i =', i)
-//i= 10 , pois o ( console.log('i =', i)) está fora do laço de repetição.
 
---------------------------------------------------------------------------
-Var em loop 2 
+/*0
+ *1
+ *2
+ *3
+ *4
+ *5
+ *6
+ *7
+ *8
+ *9
+ */
+
+console.log('i =', i)
+//i= 10 , pois o var nao tem escopo de bloco e ( console.log('i =', i)) esta fora do laco de repeticao.
+
+//Var em loop 2 
 
 const funcs = []
 
@@ -32,4 +33,4 @@ funcs[2]()
 //10
 funcs[8]()
 //10
-// mesmo retorno por que VAR NÃO TEM ESCOPO DE BLOCO!!!
+// mesmo retorno por que VAR NAO TEM ESCOPO DE BLOCO!!!
