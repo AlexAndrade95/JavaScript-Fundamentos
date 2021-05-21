0315e7563e9635aa392ac40e8903599d682c965c
AlexAndrade95 <alexneves19955@gmail.com>
2021-05-20 22:05:06 -0300

    commit 24

---
+++ 32Relacionais.js
@@ -0,0 +1,36 @@
+/*Operadores relacionais 
+  *usa-se o == para igual ou === para estritamente igual que leva em consideracao o tipo das variaveis
+  *são operadores binarios
+  */
+
+  console.log('01)', '1' == 1)
+  //true
+  console.log('02)', '1' === 1)
+  //false
+  console.log('03)', '3' != 3)
+  //false
+  console.log('04)', '3' !== 3)
+  //true
+
+  console.log('05)', 3 < 2)
+  //false
+  console.log('06)', 3 > 2)
+  //true
+  console.log('07)', 3 <= 2)
+  //false
+  console.log('08)', 3 >= 2)
+  //true
+
+  const d1 = new Date(0)
+  const d2 = new Date(0)
+  console.log('09)', d1 === d2)
+  //false
+  console.log('10)', d1 == d2)
+  //false
+  console.log('11)', d1.getTime() === d2)
+  //false
+
+  console.log('12)', undefined == null)
+  //true
+  console.log('13)', undefined === null)
+  //false
